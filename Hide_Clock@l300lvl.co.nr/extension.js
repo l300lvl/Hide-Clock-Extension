10d12b3982be7d85b64af3e61f740879fc1e6d23
l300lvl <atz3@yahoo.ca>
2012-09-25 21:13:52 -0500

    3.6 is here...for real this time!

--- Hide_Clock@l300lvl.co.nr/extension.js
+++ Hide_Clock@l300lvl.co.nr/extension.js
@@ -1,12 +1,31 @@
-const Main = imports.ui.main.panel;
+const Config = imports.misc.config
+const Main = imports.ui.main
 
-function init() {
+function init(metadata) {
+    let current_version = Config.PACKAGE_VERSION.split('.')
+    if (current_version.length != 3 || current_version[0] != 3) throw new Error("Strange version number (extension.js:9).")
+    
+    switch (current_version[1]) {
+        case"3": global.log("Warning of extension [" + metadata.uuid + "]:\n              Old development release detected (" + Config.PACKAGE_VERSION + "). You should upgrade!\n")   //eak
+        case"4": age = "old"
+            break
+        case"5": global.log("Warning of extension [" + metadata.uuid + "]:\n              Development release detected (" + Config.PACKAGE_VERSION + "). Loading as a 3.6 release.\n") //eak
+        case"6": age = "new"
+            break
+        default: throw new Error("Strange version number (extension.js:18).")
+    }
 }
 
 function disable() {
-    Main._dateMenu.actor.show();
+    let thing
+    if (age=="old") menu = Main.panel._dateMenu
+    else            menu = Main.panel.statusArea.dateMenu
+    menu.actor.show()
 }
 
 function enable() {
-    Main._dateMenu.actor.hide();
+    let thing
+    if (age=="old") menu = Main.panel._dateMenu
+    else            menu = Main.panel.statusArea.dateMenu
+    menu.actor.hide()
 }
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A very simple extension to hide the panel clock. This functions best by hiding the clock in the center, and *might* or *might not* work with the Windows List or Move Clock.
 
+Much credit to Obsidien for his help and code skills, I suggest you check out his amazing work:
+
+https://github.com/Obsidien
+
 
 ### 1 click install from E.G.O:
 
